06c2b94dc2075d9d9efb8b7fcdaaa8dc1ec7490a
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-04-07 23:52:00 +0200

    tiktaktoe in progress

--- lab2/zad2/dist/bundle.js
+++ lab2/zad2/dist/bundle.js
@@ -7,26 +7,71 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/Board.ts":
+/*!**********************!*\
+  !*** ./src/Board.ts ***!
+  \**********************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+eval("\r\nexports.__esModule = true;\r\nexports.Board = void 0;\r\nvar Cell_1 = __webpack_require__(/*! ./Cell */ \"./src/Cell.ts\");\r\nvar Board = /** @class */ (function () {\r\n    function Board(size) {\r\n        var _this = this;\r\n        this.currentSymbol = 1;\r\n        this.cells = new Array(size);\r\n        var table = document.getElementById(\"tictactoe\");\r\n        var i = 0;\r\n        for (var r = 0; r < size; r++) {\r\n            var row = table.insertRow(r);\r\n            var _loop_1 = function (c) {\r\n                var cell = row.insertCell(c);\r\n                cell.className = \"cell\";\r\n                var newCell = new Cell_1.Cell(cell);\r\n                this_1.cells[i] = newCell;\r\n                cell.addEventListener(\"click\", function () { return _this.makeMove(newCell); }, false);\r\n                i++;\r\n            };\r\n            var this_1 = this;\r\n            for (var c = 0; c < size; c++) {\r\n                _loop_1(c);\r\n            }\r\n        }\r\n    }\r\n    Board.prototype.makeMove = function (cell) {\r\n        cell.setCellValue(this.currentSymbol);\r\n        this.currentSymbol = this.currentSymbol === 1 ? -1 : 1;\r\n        cell.fillCell(this.currentSymbol);\r\n    };\r\n    return Board;\r\n}());\r\nexports.Board = Board;\r\n\n\n//# sourceURL=webpack:///./src/Board.ts?");
+
+/***/ }),
+
+/***/ "./src/Cell.ts":
+/*!*********************!*\
+  !*** ./src/Cell.ts ***!
+  \*********************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\r\nexports.__esModule = true;\r\nexports.Cell = void 0;\r\nvar Cell = /** @class */ (function () {\r\n    function Cell(cell) {\r\n        this.htmlElement = cell;\r\n    }\r\n    Cell.prototype.setCellValue = function (value) {\r\n        switch (value) {\r\n            case -1:\r\n                this.htmlElement.textContent = 'O';\r\n                break;\r\n            case 1:\r\n                this.htmlElement.textContent = 'X';\r\n                break;\r\n            default:\r\n                this.htmlElement.textContent = '';\r\n                break;\r\n        }\r\n    };\r\n    Cell.prototype.fillCell = function (value) {\r\n        this.htmlElement.style.backgroundColor = value === 1 ? '#BBBAFF' : '#F0B2D3';\r\n    };\r\n    return Cell;\r\n}());\r\nexports.Cell = Cell;\r\n\n\n//# sourceURL=webpack:///./src/Cell.ts?");
+
+/***/ }),
 
 /***/ "./src/app.ts":
 /*!********************!*\
   !*** ./src/app.ts ***!
   \********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack:///./src/app.ts?");
+eval("\r\nexports.__esModule = true;\r\nvar Board_1 = __webpack_require__(/*! ./Board */ \"./src/Board.ts\");\r\nvar board = new Board_1.Board(3);\r\n\n\n//# sourceURL=webpack:///./src/app.ts?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/app.ts"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.ts");
 /******/ 	
 /******/ })()
 ;
--- lab2/zad2/src/Cell.ts
+++ lab2/zad2/src/Cell.ts
@@ -1,14 +1,25 @@
 export class Cell {
- cellValue: number;
- htmlElement: HTMLElement;
- constructor(cell: HTMLElement) {
- this.htmlElement = cell;
+  cellValue: number ;
+  htmlElement: HTMLElement;
+  
+  constructor(cell: HTMLElement) {
+    this.htmlElement = cell;
+  }
+ 
+  setCellValue(value: number) {
+    switch(value){
+      case -1:
+        this.htmlElement.textContent = 'O'
+        break;
+      case 1:
+        this.htmlElement.textContent = 'X'
+        break;
+      default:
+        this.htmlElement.textContent = ''
+        break;
+    }
+  }
+  fillCell(value: number){
+    this.htmlElement.style.backgroundColor = value === 1 ? '#BBBAFF' : '#F0B2D3';
+  }
  }
-
- setCellValue(value: number) {
-// TODO: napisz samemu metodę, która ustawia w
- // tabeli kółko i krzyżyk. Możesz przyjąć, że kółko
- // to wartość -1, krzyżyk to wartość 1, a 0 (zero)
- // oznacza pole nieustawione.
- }
-}
